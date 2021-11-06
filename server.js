d0b1bb0c9e0ed98a41c2ed4f84fa8a0f42ddb46f
Aditya Kumar <adityak007mr@gmail.com>
2021-11-06 20:10:46 +0530

    docker build added

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+if (process.env.APP_ENV === "prod") {
+    app.use('/enviro', express.static(path.join(__dirname, 'client', 'build')))
+    app.get('/enviro/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 require('./app/routes/routes')(app)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
