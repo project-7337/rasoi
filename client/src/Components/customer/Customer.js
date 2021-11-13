661077b9970ad634d2653de65b56b5ae3d41ecf3
skullcandy69 <C@nstrue1>
2021-11-13 14:11:18 +0530

    customer ui changes

--- client/src/Components/customer/Customer.js
+++ client/src/Components/customer/Customer.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import {makeStyles} from "@material-ui/core/styles";
-import {Box, Button, Card, CardContent, CardMedia, Divider, Grid, Paper, Typography} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Button, Card, CardContent, CardMedia, Divider, Grid, Paper, Typography } from "@material-ui/core";
 import Carousel from "react-material-ui-carousel";
 import '../../styles/styles.css';
-
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1
@@ -18,12 +30,35 @@ const useStyles = makeStyles(theme => ({
     },
     divider: {
         margin: theme.spacing(3, 0)
-    }
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
 }))
+
+
 
 export default function Customer() {
     const classes = useStyles()
+    const [expanded, setExpanded] = React.useState(false);
 
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
     return (
         <div className={classes.root}>
             <Carousel
@@ -35,8 +70,78 @@ export default function Customer() {
                     })
                 }
             </Carousel>
-            <Divider className={classes.divider}>
-            </Divider>
+            <Card className={classes.root}>
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar}>
+                            R
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+                <CardMedia
+                    className={classes.media}
+                    image="/static/images/cards/paella.jpg"
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        This impressive paella is a perfect party dish and a fun meal to cook together with your
+                        guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph>
+                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                            minutes.
+                        </Typography>
+                        <Typography paragraph>
+                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                        </Typography>
+                        <Typography paragraph>
+                            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                            minutes more. (Discard any mussels that don’t open.)
+                        </Typography>
+                        <Typography>
+                            Set aside off of the heat to let rest for 10 minutes, and then serve.
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
         </div>
     )
 }
@@ -107,47 +212,32 @@ const Banner = (props) => {
 
 const items = [
     {
-        Name: "Electronics",
-        Caption: "Electrify your friends!",
+        Name: "DEAL OF THE DAY",
+        Caption: "Grab it now",
         contentPosition: "left",
         Items: [
             {
-                Name: "Macbook Pro",
-                Image: "https://source.unsplash.com/featured/?macbook"
+                Name: "Dal Makhni",
+                Image: "https://www.vegrecipesofindia.com/wp-content/uploads/2015/01/dal-makhani-recipe-1.jpg"
             },
             {
-                Name: "iPhone",
-                Image: "https://source.unsplash.com/featured/?iphone"
+                Name: "Paneer butter masala",
+                Image: "https://www.ruchiskitchen.com/wp-content/uploads/2020/12/Paneer-butter-masala-recipe-3-500x500.jpg"
             }
         ]
     },
     {
-        Name: "Home Appliances",
-        Caption: "Say no to manual home labour!",
+        Name: "Top Picks",
+        Caption: "We know whats best for you",
         contentPosition: "middle",
         Items: [
             {
-                Name: "Washing Machine WX9102",
-                Image: "https://source.unsplash.com/featured/?washingmachine"
+                Name: "khandani Pakode",
+                Image: "https://media-cdn.tripadvisor.com/media/photo-s/16/b2/35/30/photo0jpg.jpg"
             },
             {
-                Name: "Learus Vacuum Cleaner",
-                Image: "https://source.unsplash.com/featured/?vacuum,cleaner"
-            }
-        ]
-    },
-    {
-        Name: "Decoratives",
-        Caption: "Give style and color to your living room!",
-        contentPosition: "right",
-        Items: [
-            {
-                Name: "Living Room Lamp",
-                Image: "https://source.unsplash.com/featured/?lamp"
-            },
-            {
-                Name: "Floral Vase",
-                Image: "https://source.unsplash.com/featured/?vase"
+                Name: "Bittu Tikki Vala",
+                Image: "https://i.ytimg.com/vi/3MgsACjRq9E/hqdefault.jpg"
             }
         ]
     }
--- client/src/navbar/Navbar.js
+++ client/src/navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import clsx from 'clsx'
-import {AppBar, Badge, Button, IconButton, Toolbar, Typography} from "@material-ui/core"
+import { AppBar, Badge, Button, IconButton, Toolbar, Typography } from "@material-ui/core"
 import { makeStyles } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu'
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Main from "../Components/Main";
-
+import MyLocationIcon from '@material-ui/icons/MyLocation';
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
@@ -21,6 +21,10 @@ const useStyles = makeStyles(theme => ({
         position: 'fixed',
         right: '1%'
     },
+    location:{
+        margin:'auto',
+        position: 'center'
+    }
 }))
 
 export default function Navbar() {
@@ -42,6 +46,19 @@ export default function Navbar() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Rasoi
                     </Typography>
+                    <div className={classes.location}>
+                    <IconButton
+                        edge="end"
+                        aria-label="account of current user"
+                        aria-haspopup="true"
+                        color="inherit"
+                    >
+                        <MyLocationIcon />
+                    </IconButton>
+                    <Typography variant="h10" component="div" sx={{ flexGrow: 1 }}>
+                        Kalkaji Double storey new delhi
+                    </Typography>
+                    </div>
                     <div className={classes.sectionDesktop}>
                         <IconButton aria-label="show 17 new notifications" color="inherit">
                             <Badge badgeContent={17} color="secondary">
