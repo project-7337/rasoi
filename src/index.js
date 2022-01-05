284889be39f6dbe69b2e80f2093788339be9bbc0
skullcandy69 <C@nstrue1>
2022-01-06 00:42:29 +0530

    User Profile fetch and display  data from backend

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from './Components/login/Login'
-
+import UserProfile from './Components/customer/UserProfile';
 require('dotenv').config()
 
 render((<BrowserRouter>
     <Switch>
 		<Route path={process.env.REACT_APP_PUBLIC_URL + "/login"} component={Login} />
+		<Route path={process.env.REACT_APP_PUBLIC_URL + "/UserProfile"} component={UserProfile} />
         <Route component={App}/>
     </Switch>
 </BrowserRouter>), document.getElementById('root'))
